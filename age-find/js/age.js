4824048a5e0c56b38e845435532d7bafa24a2940
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:46:10 +0530

    age-finding.

---
+++ age-find/js/age.js
@@ -0,0 +1,10 @@
+//age program
+
+// n=prompt("enter the date mm-dd-yyyy")
+// date = new Date()
+// currYear=date.getFullYear()
+// //console.log(cy)
+// const bY = new Date(n);
+// const year = bY.getFullYear(); 
+// age=currYear-year;
+// console.log( `your age is : ${age}`)
